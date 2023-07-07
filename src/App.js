01398c486b91723e33516a12adb106c1094c0de5
Chris Min <chrismin@Bu.edu>
2023-07-07 00:06:17 -0400

    Initialized components and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import "./App.css"
+import React from 'react';
+import "./App.css";
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path = "/" element ={<Home />}/>
+        <Route path = "/exercise/:id " element ={<ExerciseDetail />}/>
+      </Routes>
+    </Box>
   )
 }
 
